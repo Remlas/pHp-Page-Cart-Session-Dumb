6e54b0cc496bc0169e2e6a00d87c5c71b0e4aa36
Remlas <remlas.bond@gmail.com>
2018-05-31 21:05:22 +0200

    OpenGraph tags

--- header.php
+++ header.php
@@ -5,4 +5,21 @@
 		<meta charset="UTF-8">
 		<meta name="author" content="Remlas">
 		<link rel="stylesheet" href="style.css">
+
+		<link rel="apple-touch-icon" sizes="180x180" href="/assets/favicons/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicons/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicons/favicon-16x16.png">
+		<link rel="manifest" href="/assets/favicons/site.webmanifest">
+		<link rel="mask-icon" href="/assets/favicons/safari-pinned-tab.svg" color="#5bbad5">
+		<link rel="shortcut icon" href="/assets/favicons/favicon.ico">
+		<meta name="msapplication-TileColor" content="#ffc40d">
+		<meta name="msapplication-config" content="/assets/favicons/browserconfig.xml">
+		<meta name="theme-color" content="#ffffff">
+
+		<meta property="og:image:width" content="279">
+		<meta property="og:image:height" content="279">
+		<meta property="og:description" content="Idealne rozwiązanie robione na ostatni dzień z brakiem szczeg&oacute;lnych chęci do klepania w PHP, aby podciągnąć sobie średnią, bo zabraknie do paska...">
+		<meta property="og:title" content="<?php echo $title ?> - Strona z koszykiem">
+		<meta property="og:url" content="http://remlas.pl/koszyk">
+		<meta property="og:image" content="http://remlas.pl/koszyk/assets/fb-og/og-image.jpg">
 	</head>
